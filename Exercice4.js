847b7104deb4b42fd4812caa53ca6ec576f3155c
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-18 16:11:43 +0200

    Create Exercice4.js

---
+++ Exercice4.js
@@ -0,0 +1,17 @@
+// Écrivez un programme qui demande à l'utilisateur de saisir 3 nombres puis trouver le plus grand
+
+let read = require("readline-sync");
+
+
+let nombre = read.questionFloat("Veuillez saisir un nombre: ");
+let i = 0;
+
+while (i < 2) {
+    i++
+    let temp = read.questionFloat("Veuillez saisir un nombre: ");
+    if (nombre < temp){
+        nombre = temp;
+    }
+}
+
+console.log(`Le nombre le plus grand que vous avez saisi est le ${nombre}`);
