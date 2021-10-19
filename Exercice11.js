ec9234e14f22421396d6e66238e0301e44339f89
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 15:09:52 +0200

    create exercice11.js

---
+++ Exercice11.js
@@ -0,0 +1,23 @@
+// Écrivez un programme qui demande à l'utilisateur de saisir un nombre décimal.
+// Le programme doit renvoyer exactement le nombre de billets et de pièces qu'il faut pour obtenir ce nombre
+// Resultat attendu:
+// (ex : nombre = 2437,57€)
+// Billet 500€ : 4
+// Billet 200€ : 2
+// Billet 20€ : 1
+// Billet 10€ : 1
+// Billet 5€ : 1
+// Pièce 2€ : 1
+// Pièce 0,50€ : 1
+// Pièce 0,02€ : 1
+
+let read = require("readline-sync");
+
+let caisse = () => {
+    let nombre = read.questionFloat("Veuillez saisir un nombre decimal: ")
+    let 
+
+
+}
+
+caisse();
