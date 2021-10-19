2e455e50d28d5d43663b686667cc193a6dc5feed
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 09:37:47 +0200

    update exercice6

--- Exercice6.js
+++ Exercice6.js
@@ -1,5 +1,11 @@
 // Écrivez un programme qui simulera une calculatrice et demande à l'utilisateur de saisir un premier opérande, un opérateur et un second opérande.
 // Ce programme exécutera des opérations arithmétiques de base (addition, soustraction, multiplication et division selon l'opérateur saisi
+// Resultat attendu:
+// [12, "+", 3] = 15
+// [12, "*", 3] = 36
+// [12, "/", 3] = 4
+// [12, "-", 3] = 9
+
 
 let read = require("readline-sync");
 
