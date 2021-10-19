b8002cab4a1a720eb008e2267358fdaa8f827772
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 10:13:49 +0200

    update

--- Exercice7.js
+++ Exercice7.js
@@ -4,3 +4,13 @@
 // (ex : nombre = 5)
 // 1+2+3+4+5 = 15
 
+let read = require("readline-sync");
+
+let nombre = read.questionFloat("Veuillez saisir un nombre positive: ");
+let somme = 0;
+
+for (var i = 0; i < nombre+1; i++){ // "nombre+1" j'ai pas trouver plus propre car il manquait un tour de boucle.
+    somme = somme + i
+}
+
+console.log(`La somme de tous les nombre de ${nombre} est de ${somme}.`)
