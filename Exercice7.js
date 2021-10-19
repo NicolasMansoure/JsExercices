71e4f291e99634a8b0c8647ff595aa692762a8d2
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 10:00:34 +0200

    create exercice7

---
+++ Exercice7.js
@@ -0,0 +1,6 @@
+// Écrivez un programme qui demande à l'utilisateur de saisir un nombre "N".
+// Calculer la somme de tous les nombres entre 1 et N
+// Resultat attendu:
+// (ex : nombre = 5)
+// 1+2+3+4+5 = 15
+
