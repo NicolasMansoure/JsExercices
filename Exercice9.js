394de58dbb40f99dd89279e49737e16189953c1d
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 11:45:09 +0200

    update

--- Exercice9.js
+++ Exercice9.js
@@ -11,3 +11,23 @@
 // **
 // *
 
+let read = require("readline-sync");
+let nombre = read.questionFloat("Veuillez saisir un nombre positive: ");
+let compteur = 1;
+let etoiles = "";
+
+let sapin = (nombre, compteur, etoiles) => {
+    for (let i = 0; i < nombre; i++) {
+        if (compteur <= nombre) {
+            etoiles += "*";
+            console.log(etoiles);
+            compteur++
+        }   
+    }
+    for (let j=compteur; compteur>0; j--){
+        etoiles = etoiles.substring(1);
+        console.log(etoiles);
+        compteur--
+    }
+}
+sapin(nombre,compteur,etoiles);
