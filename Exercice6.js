1821db4ccf413814979923c4c7754cdfba8316c3
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 09:56:57 +0200

    update exercice6

--- Exercice6.js
+++ Exercice6.js
@@ -6,8 +6,34 @@
 // [12, "/", 3] = 4
 // [12, "-", 3] = 9
 
-
 let read = require("readline-sync");
 
+let operande1 = read.questionFloat("Veuillez saisir la premiere operande: ");
+let operateur = read.question("Veuillez saisir l'operateur (addition: +, soutraction: -, division! /, multiplication: *): ");
+let operande2 = read.questionFloat("Veuillez saisir la seconde operande: ");
+let resultat = 0;
 
+let calculatrice = (operande1, operateur, operande2) => {
+    if (operateur == "+") {
+        resultat = operande1 + operande2
+        console.log(`${operande1} ${operateur} ${operande2} = ${resultat}`)
+    }
+    else if (operateur == "-") {
+        resultat = operande1 - operande2
+        console.log(`${operande1} ${operateur} ${operande2} = ${resultat}`)
+    }
+    else if (operateur == "/") {
+        resultat = operande1 / operande2
+        console.log(`${operande1} ${operateur} ${operande2} = ${resultat}`)
+    }
+    else if (operateur == "*") {
+        resultat = operande1 * operande2
+        console.log(`${operande1} ${operateur} ${operande2} = ${resultat}`)
+    }
+    else {
+        console.log("Erreur");
+    }
+}
+
+calculatrice(operande1,operateur,operande2);
 
