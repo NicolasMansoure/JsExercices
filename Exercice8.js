1f0f221b0b0fb7d0eddf26e8c59ca372bf711a0b
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 10:14:55 +0200

    create exercice8.js

---
+++ Exercice8.js
@@ -0,0 +1,10 @@
+// Écrivez un programme qui demande à un utilisateur un nombre puis qui génère un escalier d'étoiles.
+// Resultat attendu:
+// (ex : nombre = 5)
+// *
+// **
+// ***
+// ****
+// *****
+
+
