5af1235e7c7bae16f4301c8542721264ce47a097
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 13:49:36 +0200

    update solution

--- Exercice9.js
+++ Exercice9.js
@@ -31,3 +31,24 @@ let sapin = (nombre, compteur, etoiles) => {
     }
 }
 sapin(nombre, compteur, etoiles);
+
+
+
+// A U T R E    S O L U T I O N
+// ___________________________________________________
+// let read = require("readline-sync");
+// const SYMBOLE = "*";
+// function Triangle(nbLignes) {
+//     let strTriangle = "";
+//     for (let i = 1; i <= nbLignes; i++) {
+//         strTriangle = SYMBOLE.repeat(i);
+//         console.log(strTriangle);
+//     }
+//     for (let j = nbLignes; j > 0; j--) {
+//         strTriangle = SYMBOLE.repeat(j);
+//         console.log(strTriangle);
+//     }
+// }
+// let n = read.questionInt("Entrez un nombre Entier :");
+// n = parseInt(n);
+// Triangle(n)
