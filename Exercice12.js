25ed90b2a2a952d870ee21ad339907bb33de1abc
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2021-10-19 17:16:22 +0200

    create exercice12.js

---
+++ Exercice12.js
@@ -0,0 +1,8 @@
+// Vous disposez d'une liste de températures (°C).
+// Parcourez cette liste et affichez la température la plus proche de 0°C. 
+// Si une température négative est aussi proche de 0 qu'une température positive, la valeur négative prend le dessus.
+// Resultat attendu:
+// [12, 25, 5, 7, 6, -5]
+// La température la plus proche de 0°C est -5°C.
+
+
